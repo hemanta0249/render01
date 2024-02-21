e2f0c333c657605a0a6a62016a457301df467fef
hemant <hemantarora091@gmail.com>
2024-02-21 18:11:25 +0530

    h

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
     const handleSubmit = async (e)=>{
         e.preventDefault();
 
-        const response = await fetch("localhost:8000/api/auth/login",{
+        const response = await fetch("http://localhost:8000/api/auth/login",{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
